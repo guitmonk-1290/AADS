43cd0468c60bc3485f93c03016e261d951bbd980
adi <104564065+guitmonk-1290@users.noreply.github.com>
2025-08-21 08:37:23 +0530

    Create Bucket_Sort.cpp

---
+++ Bucket_Sort.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector<float>& bucket) {
+    for (int i = 1; i < bucket.size(); i++) {
+        float key = bucket[i];
+        int j = i - 1;
+        while (j >= 0 && bucket[j] > key) {
+            bucket[j + 1] = bucket[j];
+            j--;
+        }
+        bucket[j + 1] = key;
+    }
+}
+
+void bucketSort(vector<float> &arr, int n) {
+    vector<float> b[n];
+    
+    for (int i = 0; i < n; i++) {
+        int bi = floor(n * arr[i]);
+        b[bi].push_back(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        insertionSort(b[i]);
+    }
+
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < b[i].size(); j++) {
+            arr[index++] = b[i][j];
+        }
+    }
+}
+
+int main() {
+    vector<float> arr = {0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bucketSort(arr, n);
+
+    cout << "Sorted array is \n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
