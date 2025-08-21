5de307ba04634ff8a6f589f6c4e9342bd032d9fc
adi <104564065+guitmonk-1290@users.noreply.github.com>
2025-08-21 09:47:28 +0530

    Create AVL_deletion.cpp

---
+++ AVL_deletion.cpp
@@ -0,0 +1,211 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int key;
+    Node *left;
+    Node *right;
+    int height;
+
+    Node(int k) {
+        key = k;
+        left = nullptr;
+        right = nullptr;
+        height = 1;
+    }
+};
+
+int height(Node *N) {
+    if (N == nullptr)
+        return 0;
+    return N->height;
+}
+
+Node *rightRotate(Node *y) {
+    Node *x = y->left;
+    Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = 1 + max(height(y->left),
+                height(y->right));
+    x->height = 1 + (height(x->left),
+                height(x->right));
+
+    return x;
+}
+
+Node *leftRotate(Node *x) {
+    Node *y = x->right;
+    Node *T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = 1 + max(height(x->left),
+                height(x->right));
+    y->height = 1 + max(height(y->left),
+                height(y->right));
+
+    return y;
+}
+
+// Balance factor of node N
+int getBalance(Node *N) {
+    if (N == nullptr)
+        return 0;
+    return height(N->left) - 
+           height(N->right);
+}
+
+Node* insert(Node* node, int key) {
+    if (node == nullptr)
+        return new Node(key);
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else 
+        return node;
+
+    node->height = 1 + max(height(node->left), 
+                   height(node->right));
+
+    int balance = getBalance(node);
+
+    // Left Left Case
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    // Right Right Case
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    // Left Right Case
+    if (balance > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    // Right Left Case
+    if (balance < -1 && key < node->right->key) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+  
+    return node;
+}
+
+Node *findMinvalNode(Node* node) {
+    Node* current = node;
+
+    while (current->left != nullptr)
+        current = current->left;
+
+    return current;
+}
+
+Node* deleteNode(Node* root, int key) {
+    if (root == nullptr)
+        return root;
+
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+
+    else {
+        // only one child or no child
+        if ((root->left == nullptr) || 
+            (root->right == nullptr)) {
+            Node *temp = root->left ? 
+                         root->left : root->right;
+
+            // No child
+            if (temp == nullptr) {
+                temp = root;
+                root = nullptr;
+            } else // One child
+                *root = *temp;
+            free(temp);
+        } else {
+            // finding the inorder successor
+            Node* temp = findMinValNode(root->right);
+
+            root->key = temp->key;
+
+            root->right = deleteNode(root->right, temp->key);
+        }
+    }
+
+    if (root == nullptr)
+        return root;
+
+    root->height = 1 + max(height(root->left), 
+                   height(root->right));
+
+    int balance = getBalance(root);
+
+
+    // Left Left Case
+    if (balance > 1 && 
+        getBalance(root->left) >= 0)
+        return rightRotate(root);
+
+    // Left Right Case
+    if (balance > 1 && 
+        getBalance(root->left) < 0) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    // Right Right Case
+    if (balance < -1 && 
+        getBalance(root->right) <= 0)
+        return leftRotate(root);
+
+    // Right Left Case
+    if (balance < -1 && 
+        getBalance(root->right) > 0) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+void preOrder(Node *root) {
+    if (root != nullptr) {
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+// Driver Code
+int main() {
+    Node *root = nullptr;
+
+    root = insert(root, 9);
+    root = insert(root, 5);
+    root = insert(root, 10);
+    root = insert(root, 0);
+    root = insert(root, 6);
+    root = insert(root, 11);
+    root = insert(root, -1);
+
+    cout << "preorder traversal of tree: \t";
+    preOrder(root);
+
+    root = deleteNode(root, 10);
+
+    cout << "\npreorder traversal after"
+            " deletion of 10: \t";
+    preOrder(root);
+
+    return 0;
+}
